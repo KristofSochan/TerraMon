1194a39c221baaf20ce167bb81a367d06b98967a
Will Erf <willerf@Wills-MacBook-Pro.local>
2022-03-25 21:42:05 -0400

    Basic page setup

---
+++ terra-mon/src/components/BattleCard.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+function BattleCard() {
+  return (
+    <div>
+        <Link to="/battle">Battle</Link>
+    </div>
+  )
+}
+
+export default BattleCard
---
+++ terra-mon/src/pages/Landing.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+import BattleCard from '../components/BattleCard';
+
+function Landing() {
+  return (
+    <div>
+      Hello
+      <BattleCard></BattleCard>
+    </div>
+  )
+}
+
+export default Landing
